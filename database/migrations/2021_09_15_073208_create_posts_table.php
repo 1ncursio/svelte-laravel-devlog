06031daffc17e32219a14af3745b5efae1f3958d
1ncursio <ckswn1323@naver.com>
2021-09-15 16:45:43 +0900

    WIP: Generate posts mfs

---
+++ database/migrations/2021_09_15_073208_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('title', 64);
+            $table->string('slug', 64);
+            $table->text('content');
+            $table->enum('access_level', ['public', 'private', 'unlisted']);
+            $table->string('thumbnail', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('posts');
+    }
+}
